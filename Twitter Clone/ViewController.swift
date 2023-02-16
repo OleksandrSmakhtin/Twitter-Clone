373068ac743d30ea4387561922558381d4b3ee0d
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-02-16 14:48:33 +0200

    Initial Commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by Oleksandr Smakhtin on 16.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
