7bf2976a7082e02c1f03be8e3df527ab99892701
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-05-14 00:05:00 +0300

    - Add fire store

---
+++ Twitter Clone/Networking/DatabaseManager.swift
@@ -0,0 +1,34 @@
+//
+//  DataBaseManager.swift
+//  Twitter Clone
+//
+//  Created by Oleksandr Smakhtin on 13.05.2023.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+import FirebaseFirestoreCombineSwift
+import Combine
+
+class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+    // firestore instance
+    let db = Firestore.firestore()
+    // path for users in collection
+    let usersPath: String = "users"
+    
+    
+    // add user to collection
+    func collectionUsers(add user: User) -> AnyPublisher<Bool, Error> {
+        let twitterUser = TwitterUser(from: user)
+        // access collection by pass // creating new doc with id // set data
+        return db.collection(usersPath).document(twitterUser.id).setData(from: twitterUser).map { _ in
+            // if successfull true
+            return true
+        }.eraseToAnyPublisher()
+    }
+    
+}
--- Twitter Clone/ViewModels/AuthViewViewModel.swift
+++ Twitter Clone/ViewModels/AuthViewViewModel.swift
@@ -46,16 +46,35 @@ final class AuthViewViewModel: ObservableObject {
     //MARK: - Create user
     func createUser() {
         guard let email = email, let password = password else { return }
-        AuthManager.shared.registerUser(with: email, password: password).sink { [weak self] completion in
+        AuthManager.shared.registerUser(with: email, password: password)
+            // handle user output
+            .handleEvents(receiveOutput: { [weak self] user in
+                self?.user = user
+        })
+            
+            .sink { [weak self] completion in
             // handle errors
             if case .failure(let error) = completion {
                 self?.error = error.localizedDescription
             }
-            
+            // handle firestore
         } receiveValue: { [weak self] user in
-            self?.user = user
+            self?.createUserRecord(for: user)
         }.store(in: &subscriptions )
     }
+    
+    //MARK: - Create user record
+    private func createUserRecord(for user: User) {
+        DatabaseManager.shared.collectionUsers(add: user).sink { comletion in
+            if case .failure(let error) = comletion {
+                self.error = error
+            }
+        } receiveValue: { <#Bool#> in
+            <#code#>
+        }
+
+    }
+    
     
     //MARK: - Login user
     func loginUser() {
